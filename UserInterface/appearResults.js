82e450a438869aa5940937768ee6f4221ad2b5bb
Reece Martin <capt.rm.fae@gmail.com>
2017-05-27 09:55:14 -0700

    Delete appearResults.js

--- UserInterface/appearResults.js
+++
@@ -1,8 +0,0 @@
-function validate() {
-    $(".result").css("display", "block");
-    //$("div").toggleClass("resultShow");
-    //$(".result").toggleClass(".resultShow");
-    $("input").css("background-image", "none");
-    window.location = 'index.html#results';
-
-  }
