1f1853a5e1167538506473d55eac4d82f0b68365
jdawwwggo <venuzwayla@gmail.com>
2017-05-26 20:13:26 -0700

    User Interface
    This is the User Interface for the music recommender. This currently holds the homepage where the user enters the artist's name. There are other pages that will be uploaded upon completion (artists profile with a list of songs and bio / social media links). The UI is written in HTML, styled with CSS, with functions from Javascript.

---
+++ UserInterface/appearResults.js
@@ -0,0 +1,8 @@
+function validate() {
+    $(".result").css("display", "block");
+    //$("div").toggleClass("resultShow");
+    //$(".result").toggleClass(".resultShow");
+    $("input").css("background-image", "none");
+    window.location = 'index.html#results';
+
+  }
